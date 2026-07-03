afef74eb8535f8add176394b14ce045c8f37781e
orca <dev.orca04@gamil.com>
2026-07-03 22:41:07 +0900

    Feat: add isLittleEndian function to utils.h for endianess detection

--- src/utils.h
+++ src/utils.h
@@ -12,6 +12,24 @@
 #include "ownership.h"
 #include "huffman.h"
 
+static inline bool isLittleEndian(void) {
+    // uint16_t test = 0x1234;
+    // uint8_t* ptr = (uint8_t*)&test;
+    // return ptr[0] == 0x34;
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    // 1. GCC/Clang이 빌드할 때 빅 엔디안인 걸 알면
+    // 이 함수 전체를 그냥 'return false;' 라는 단 한 줄의 상수로 치환해 버립니다.
+    return false;
+#elif defined(_MSC_VER) && defined(_M_PPC)
+    // 2. 구형 MSVC 빅 엔디안 환경 대응용 안전장치
+    return false;
+#else
+    // 3. 윈도우(x86_64), 리눅스, 안드로이드 등 전 세계 99%의 리틀 엔디안 환경.
+    // build time에 바로 true 평가.
+    return true;
+#endif
+}
+
 typedef enum PrintMode {
     bin,
     ascii,
